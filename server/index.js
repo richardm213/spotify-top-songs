69cb0f3ae7bd87b396c36338f4e9a534c5d20848
Richard Meng <lemonbuttercookie@gmail.com>
2022-09-08 16:56:24 -0700

    Add login route

--- server/index.js
+++ server/index.js
@@ -2,9 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8888;
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
+const SpotifyWebApi = require("spotify-web-api-node");
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: process.env.REDIRECT_URI,
 });
 
-app.listen(port, console.log("Visit http://localhost:" + port));
+app.get("/login", (req, res) => {
+  const scopes = require("./scopes");
+  res.redirect(spotifyApi.createAuthorizeURL(scopes));
+});
+
+app.listen(port, console.log("Visit http://localhost:" + port + "/login"));
