3f75d0e43fc78512f93ce46801989996f5d687fa
Richard Meng <lemonbuttercookie@gmail.com>
2022-09-11 18:35:24 -0700

    Update App background color, font, and margin

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "../Login";
 import TrackGrid from "../TrackGrid";
+import "./style.css";
 
 const App = () => {
   const login = new URLSearchParams(window.location.search).get("login");
