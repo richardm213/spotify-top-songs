e7061e6bdb3f0326f537d8bbc3af520c04c0d39d
Richard Meng <lemonbuttercookie@gmail.com>
2022-09-13 17:49:11 -0700

    Update currTrack when user hovers over a Track

--- client/src/components/Track/Track.js
+++ client/src/components/Track/Track.js
@@ -4,10 +4,22 @@ import "./style.css";
 const Track = (props) => {
   const audio = new Audio(props.preview);
   const handleMouseOver = () => {
+    props.setCurrTrack({
+      albumName: props.albumName,
+      songName: props.songName,
+      albumCover: props.albumCover,
+      artistName: props.artistName,
+    });
     audio.currentTime = 0;
     audio.play();
   };
   const handleMouseLeave = () => {
+    props.setCurrTrack({
+      songName: "",
+      albumCover: require("../../assets/default.png"),
+      albumName: "",
+      artistName: "",
+    });
     audio.pause();
   };
   return (
