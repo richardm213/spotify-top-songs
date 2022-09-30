ab1112b8ebe7eadff1b1bde833cff40c9988a279
Richard Meng <lemonbuttercookie@gmail.com>
2022-09-30 16:35:19 -0700

    Add refresh route to refresh the access token

--- server/index.js
+++ server/index.js
@@ -44,6 +44,22 @@ app.get("/callback", async (req, res) => {
   }
 });
 
+app.post("/refresh", async (req, res) => {
+  const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI,
+    refreshToken: req.body.refreshToken,
+  });
+  try {
+    const tokens = await spotifyApi.refreshAccessToken();
+    console.log("New access token: " + tokens.body.access_token);
+    res.json({ accessToken: tokens.body.access_token });
+  } catch (err) {
+    res.sendStatus(err.statusCode);
+  }
+});
+
 app.post("/top/:time_range/:offset", async (req, res) => {
   const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
