81dbe8856e8864c00afa01dac735192d4548e36a
Richard Meng <lemonbuttercookie@gmail.com>
2022-09-30 15:05:09 -0700

    Add Popup to introduce user to site

--- client/src/components/TopTracks/TopTracks.js
+++ client/src/components/TopTracks/TopTracks.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import Popup from "../Popup";
 import TrackGrid from "../TrackGrid";
 import TrackDetails from "../TrackDetails";
 import "./style.css";
 
 const TopTracks = () => {
+  const [popupClicked, setPopupClicked] = useState(false);
   const [currTrack, setCurrTrack] = useState({
     songName: "",
     albumCover: require("../../assets/default.png"),
@@ -12,9 +14,12 @@ const TopTracks = () => {
   });
 
   return (
-    <div id="top-tracks">
-      <TrackGrid setCurrTrack={setCurrTrack} />
-      <TrackDetails id="track-details" currTrack={currTrack} />
+    <div>
+      {!popupClicked && <Popup setClicked={setPopupClicked} />}
+      <div id="top-tracks">
+        <TrackGrid setCurrTrack={setCurrTrack} />
+        <TrackDetails id="track-details" currTrack={currTrack} />
+      </div>
     </div>
   );
 };
