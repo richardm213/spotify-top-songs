23892e86bfffb75472a9471ba67308fa5fa199b9
Richard Meng <lemonbuttercookie@gmail.com>
2022-09-30 16:39:40 -0700

    Update useAuth to refresh accessToken based on expiresIn

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,14 +1,33 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const useAuth = () => {
   const [accessToken, setAccessToken] = useState();
+  const [refreshToken, setRefreshToken] = useState();
+  const [expiresIn, setExpiresIn] = useState();
 
   useEffect(() => {
     if (accessToken) return;
     setAccessToken(
       new URLSearchParams(window.location.search).get("accessToken")
     );
+    setRefreshToken(
+      new URLSearchParams(window.location.search).get("refreshToken")
+    );
+    setExpiresIn(new URLSearchParams(window.location.search).get("expiresIn"));
   }, [accessToken]);
+
+  useEffect(() => {
+    if (!refreshToken) return;
+    const i = setInterval(() => {
+      axios
+        .post("http://localhost:8888/refresh", { refreshToken })
+        .then((data) => {
+          setAccessToken(data.accessToken);
+        });
+    }, (expiresIn / 6) * 1000);
+    return () => clearInterval(i);
+  }, [refreshToken, expiresIn]);
 
   return accessToken;
 };
