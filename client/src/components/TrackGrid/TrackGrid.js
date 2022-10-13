c4eb145493e80445209d95b885ecf9535532e166
Richard Meng <lemonbuttercookie@gmail.com>
2022-10-12 17:08:15 -0700

    Update time range dropdown to inline list

--- client/src/components/TrackGrid/TrackGrid.js
+++ client/src/components/TrackGrid/TrackGrid.js
@@ -1,4 +1,4 @@
-import React, { useState, memo } from "react";
+import React, { useState, useEffect, memo } from "react";
 import useTrackData from "../../hooks/useTrackData";
 import "./style.css";
 
@@ -11,19 +11,31 @@ const TrackGrid = (props) => {
     props.setCurrTrack
   );
 
+  useEffect(() => {
+    document.getElementById("time_range").children[0].style.color =
+      "var(--time-range-selected)";
+  }, []);
+
   const handleTimeRange = (e) => {
-    setTimeRange(e.target.value);
+    setTimeRange(e.target.getAttribute("value"));
+    const a = document.getElementById("time_range").children;
+    for (let i = 0; i < 3; ++i) a[i].style.color = "var(--time-range-primary)";
+    e.target.style.color = "var(--time-range-selected)";
   };
 
   return (
     <div id="track-grid">
       <h1 id="tracks-header">Your Top Tracks</h1>
       <div id="time_range">
-        <select value={timeRange} onChange={handleTimeRange}>
-          <option value="short_term">Past month</option>
-          <option value="medium_term">Past 6 months</option>
-          <option value="long_term">All time</option>
-        </select>
+        <span value="short_term" onClick={handleTimeRange}>
+          Past month
+        </span>
+        <span value="medium_term" onClick={handleTimeRange}>
+          Past 6 months
+        </span>
+        <span value="long_term" onClick={handleTimeRange}>
+          All time
+        </span>
       </div>
       <div id="tracks">{trackData}</div>
     </div>
